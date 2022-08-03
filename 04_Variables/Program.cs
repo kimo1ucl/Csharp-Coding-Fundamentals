e7cc35a4e4bace76f088ee21cb309ab2e546caa5
kimo1ucl <kimo1@ucl.dk>
2022-08-03 12:13:07 +0200

    As prepared for scope of 1. semester
    This code does not cover all the videos available in Bob Tabors C# Fundamentals for Beginners (https://www.youtube.com/watch?v=0QUgvfuKvWU)

---
+++ 04_Variables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Declaration
+            int x;
+            int y;
+            
+            //initialization
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            /*
+            Console.WriteLine("What is you name?");
+            Console.WriteLine("What is your first name?");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.WriteLine("What is your last name?");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+            */
+            
+            Console.WriteLine("What is you name?");
+            Console.WriteLine("What is your first name?");
+            string myFirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 05_Decisions/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            //assigning
+            string userValue = Console.ReadLine();
+
+
+            //Decision
+            /*
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat!";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat!";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = "Sorry, we didn't understand that...";
+                Console.WriteLine(message);
+            }
+            */
+
+            //Decision refactored,
+            //same variable message + ("bucket") used for all decision evaluations
+            /*
+            string message = "";
+
+            if (userValue == "1")
+            {
+                message = "You won a new car!";
+            }
+            else if (userValue == "2")
+            {
+                message = "You won a new boat!";
+            }
+            else if (userValue == "3")
+            {
+                message = "You won a new cat!";
+            }
+            else
+            {
+                message = "Sorry, we didn't understand that...";
+                message += "You loose";
+                Console.WriteLine(message);
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            //single condition
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You entered: {0}, therefor you won a {1}.",userValue, message);
+            Console.ReadLine();
+
+        }
+    }
+}
